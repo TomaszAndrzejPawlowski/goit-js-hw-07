86461e45613579e0f52ca7df48ef8599b7afaea4
Tomasz <pawlowski.t.93@gmail.com>
2023-08-21 21:34:19 +0200

    First task done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,50 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector(".gallery");
+
+galleryItems.forEach((picture) => {
+  gallery.insertAdjacentHTML(
+    "beforeend",
+    `<li>
+        <div class="gallery__item">
+          <a class="gallery__link" href="${picture.original}">
+            <img
+              class="gallery__image"
+              src="${picture.preview}"
+              data-source="${picture.original}"
+              alt="${picture.description}"
+            />
+          </a>
+        </div>
+      </li>`
+  );
+});
+
+for (let i = 0; i < galleryItems.length; i += 1) {
+  let imgDiv = gallery.childNodes[i].firstElementChild;
+  imgDiv.style.height = "100%";
+}
+
+const selectImage = (img) => {
+  img.preventDefault();
+  if (img.target.nodeName !== "IMG") {
+    return;
+  }
+  const selectedImg = img.target.dataset.source;
+  const instance = basicLightbox.create(`
+    <img src="${selectedImg}">
+`);
+  instance.show();
+
+  const close = (event) => {
+    if (event.code === "Escape") {
+      instance.close();
+    }
+  };
+  document.addEventListener("keydown", close);
+};
+
+gallery.addEventListener("click", selectImage);
