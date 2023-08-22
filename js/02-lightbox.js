36680e678f471a8511ca308599b72c233e60041b
Tomasz <pawlowski.t.93@gmail.com>
2023-08-22 15:47:45 +0200

    Homework finished

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,28 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector(".gallery");
+
+galleryItems.forEach((picture) => {
+  gallery.insertAdjacentHTML(
+    "beforeend",
+    `<li><a class="gallery__item" href="${picture.original}">
+  <img class="gallery__image" src="${picture.preview}" alt="${picture.description}" />
+</a></li>`
+  );
+});
+for (let i = 0; i < galleryItems.length; i++) {
+  let link = gallery.childNodes[i].firstChild;
+  link.style.height = "100%";
+  link.style.display = "block";
+}
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionSelector: "img",
+  captionType: "attr",
+  captionsData: "alt",
+  captionPosition: "bottom",
+  captionDelay: 250,
+});
